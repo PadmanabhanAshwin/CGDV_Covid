436ee7ad82102da4b5181760162c28f463717be8
meenurajapandian <meenu.shreeji@googlemail.com>
2020-09-02 18:32:07 -0400

    Header fonts

--- docs/d3plots/country_bar1.js
+++ docs/d3plots/country_bar1.js
@@ -245,6 +245,7 @@ d3.csv("https://raw.githubusercontent.com/PadmanabhanAshwin/CGDV_Covid/master/vi
 	var selector = d3.select("#drop1")
     	.append("select")
     	.attr("id","dropdown1")
+      .attr("class", "custom-select")
     	.on("change", function(d){
         	selection = document.getElementById("dropdown1");
 
